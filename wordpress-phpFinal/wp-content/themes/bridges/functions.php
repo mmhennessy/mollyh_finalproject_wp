23c7e58ced12502601b301af00d4ceb616627b2e
mmhennessy <mhennessy@mcad.edu>
2015-05-03 19:11:57 -0500

    Styles Commit
    Changes to style sheet and file changes

---
+++ wordpress-phpFinal/wp-content/themes/bridges/functions.php
@@ -0,0 +1,15 @@
+/*
+Template Name: Building Bridges
+*/
+
+<?php the_content(); ?>
+
+<?php
+	theme_activation_function;
+	get_template_part('index.pho');
+	get_template_part('stereotypes.php');
+	get_template_part('style.css');
+
+?>
+
+<?php echo "This is our custom Template!"?>
