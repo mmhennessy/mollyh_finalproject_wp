07d43f8efa2fc37b97358a753524782547991c5b
mmhennessy <mhennessy@mcad.edu>
2015-05-08 23:44:33 -0500

    Added Files
    Files added, missing from folder

---
+++ wordpress-phpFinal/wp-content/themes/bridges/stereotypes.php
@@ -0,0 +1,30 @@
+<?php get_header(); ?>
+	<!--Images from google.com representing our theme--><a href="stereotypes.php" id="" title="stereotypes">stereotypes</a>
+
+	<img src="images/adults.jpg" alt="Group of Individuals">
+	<img src="images/world.jpg" alt="Globe Image">
+
+<section class="secA">
+	<h2>Facts about Stereotypes</h2>
+
+	<!--some basic starter facts on stereotypes that affect society.-->
+		<ul>
+			<li>Stereotypes exist in all groups, including all ethnic groups</li>
+			<li>Stereotypes may include labeling or making generalized statements implying that all members of a group are a certain way.</li>
+			<li>Stereotypes can lead to major problems in society</li>
+		</ul>
+
+</section>
+
+<section class="secB">
+	<h2>Combating Stereotypes</h2>
+
+	<!--A few tips on educatiing the community about stereotypes-->
+		<ul>
+			<li>Education and raising awareness are important steps in combating stereotypes</li>
+			<li>Also, it is important to encourage open interaction with all groups, not only ethnic groups but people with disabilities as well</li>
+			<li>Lastly, people should not be afraid to ask questions and conduct research on other groups</li>
+		</ul>
+</section>
+
+<?php get_footer(); ?>
