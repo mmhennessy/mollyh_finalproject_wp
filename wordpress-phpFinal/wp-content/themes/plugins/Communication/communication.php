a7c04a3108708944dcc4b56225036b1ecc2cf438
mmhennessy <mhennessy@mcad.edu>
2015-05-10 21:05:27 -0500

    Final Commit
    Final Changes

---
+++ wordpress-phpFinal/wp-content/themes/plugins/Communication/communication.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @package Building Bridges
+ */
+/*
+Plugin Name: Building Bridges
+Plugin URI: http://localhost:8888/wordpress-phpFinal/wp-admin/edit.php?post_type=page
+Description: Building Bridges is a website designed to open communication and remove barriers to communication caused by language and cultural barriers.
+Author: Molly M. Hennessy
+Author URI: http://localhost:8888/wordpress-phpFinal/wp-admin/edit.php?post_type=page
+*/
+
